f6140cb1f0e65797ec1584e12dc32d1a12895a9e
Zachary Chai <zachary.chai@outlook.com>
2017-11-25 16:29:37 -0500

    connect to google calendar api

---
+++ lib/calendar_api/client.rb
@@ -0,0 +1,50 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+
+require 'fileutils'
+
+module CalendarApi
+  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+  APPLICATION_NAME = 'Time Tracker Script'
+  CLIENT_SECRETS_PATH = 'client_secret.json'
+  CREDENTIALS_PATH = File.join('/opt/time_tracker', '.credentials',
+                               "google-api.yaml")
+  SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+
+  class Client
+    def self.instance
+      if @client
+        return @client
+      end
+      @client = Google::Apis::CalendarV3::CalendarService.new
+      @client.client_options.application_name = APPLICATION_NAME
+      @client.authorization = authorize
+      @client
+    end
+
+    private
+
+    def self.authorize
+      FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+      client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+      token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+      authorizer = Google::Auth::UserAuthorizer.new(
+        client_id, SCOPE, token_store)
+      user_id = 'default'
+      credentials = authorizer.get_credentials(user_id)
+      if credentials.nil?
+        url = authorizer.get_authorization_url(
+          base_url: OOB_URI)
+        puts "Open the following URL in the browser and enter the " +
+             "resulting code after authorization"
+        puts url
+        code = gets
+        credentials = authorizer.get_and_store_credentials_from_code(
+          user_id: user_id, code: code, base_url: OOB_URI)
+      end
+      credentials
+    end
+  end
+end
--- lib/time_tracker.rb
+++ lib/time_tracker.rb
@@ -1,5 +1,21 @@
 require "time_tracker/version"
 
+require "calendar_api/client"
+
 module TimeTracker
-  # Your code goes here...
+  calendar_api = CalendarApi::Client.instance
+
+  calendar_id = 'primary'
+  response = calendar_api.list_events(calendar_id,
+                                 max_results: 10,
+                                 single_events: true,
+                                 order_by: 'startTime',
+                                 time_min: Time.now.iso8601)
+
+  puts "Upcoming events:"
+  puts "No upcoming events found" if response.items.empty?
+  response.items.each do |event|
+    start = event.start.date || event.start.date_time
+    puts "- #{event.summary} (#{start})"
+  end
 end
