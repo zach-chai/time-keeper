934a30a10be1db6d3e91b6d76fd7259261504484
Zachary Chai <zachary.chai@outlook.com>
2017-12-03 16:31:31 -0500

    parse calendar and harvest to events

--- lib/time_tracker.rb
+++ lib/time_tracker.rb
@@ -8,32 +8,62 @@ require "harvest_api/client"
 require "time_tracker/version"
 require "time_tracker/event"
 
+
 module TimeTracker
-  harvest_api = HarvestApi::Client.instance
-  calendar_api = CalendarApi::Client.instance
-  calendar_id = 'primary'
+  PROJECT_ID = 12499555
 
-  response = calendar_api.list_events(calendar_id,
-                                      max_results: 25,
-                                      single_events: true,
-                                      order_by: 'startTime',
-                                      time_min: Time.now.beginning_of_week.iso8601,
-                                      time_max: Time.now.iso8601)
+  MEETING_TASK_ID = 6998455
 
-  time_events = []
-  response.items.each do |event|
-    time_events << TimeTracker::Event.build_from(event)
+  class << self
+    def harvest_api
+      HarvestApi::Client.instance
+    end
+
+    def calendar_api
+      CalendarApi::Client.instance
+    end
+
+    def calendar_events opts = {}
+      opts[:start_time] ||= Time.now.beginning_of_week.iso8601
+      opts[:end_time] ||= Time.now.iso8601
+      opts[:calendar_id] ||= 'primary'
+
+      res = calendar_api.list_events(opts[:calendar_id],
+                                    max_results: 25,
+                                    single_events: true,
+                                    order_by: 'startTime',
+                                    time_min: opts[:start_time],
+                                    time_max: opts[:end_time])
+      events = []
+      res.items.each do |event|
+        events << TimeTracker::Event.build_from(event)
+      end
+      events
+    end
+
+    def harvest_events opts = {}
+      opts[:start_time] ||= Time.now.beginning_of_week.iso8601
+      opts[:end_time] ||= Time.now.iso8601
+
+      res = harvest_api.time_entries.all(from: opts[:start_time],
+                                        to: opts[:end_time])
+
+      meetings = res.select {|te| te.task['id'] == MEETING_TASK_ID}
+      Time.zone = 'America/Toronto'
+      events = []
+      meetings.each do |event|
+        events << TimeTracker::Event.build_from(event)
+      end
+      events
+    end
   end
 end
 
-# calendar_api = CalendarApi::Client.instance
+# h_events = TimeTracker.harvest_events start_time: Time.now.beginning_of_day.iso8601
 #
-# calendar_id = 'primary'
-# response = calendar_api.list_events(calendar_id, max_results: 10, single_events: true, order_by: 'startTime', time_min: Time.now.iso8601)
-#
-# puts "Upcoming events:"
-# puts "No upcoming events found" if response.items.empty?
-# response.items.each do |event|
-#   start = event.start.date || event.start.date_time
-#   puts "- #{event.summary} (#{start})"
-# end
+# c_events = TimeTracker.calendar_events start_time: Time.now.beginning_of_day.iso8601
+# TimeTracker.create_harvest_events events
+
+# TimeTracker.sync_events
+
+# puts events
--- lib/time_tracker/event.rb
+++ lib/time_tracker/event.rb
@@ -1,19 +1,46 @@
 module TimeTracker
   class Event
-    attr_accessor :start_time, :end_time, :title, :description
+    attr_accessor :task_id, :start_time, :end_time, :date, :duration, :title, :description
 
     def self.build_from(external_event)
       if external_event.is_a? Google::Apis::CalendarV3::Event
         event = self.new
+        event.task_id = MEETING_TASK_ID
         event.title = external_event.summary
-        event.start_time = external_event.start.date_time
-        event.end_time = external_event.end.date_time
+        event.start_time = Time.parse(external_event.start.date_time.iso8601)
+        event.end_time = Time.parse(external_event.end.date_time.iso8601)
+        event
+      elsif external_event.is_a? Harvest::Resource::TimeEntry
+        event = self.new
+        event.task_id = external_event.task['id']
+        event.title = external_event.notes
+        event.date = external_event.spent_date
+        event.duration = external_event.hours
         event
       end
     end
 
-    def duration
-      end_time.min - start_time.min
+    def spent_date
+      if date
+        date
+      else
+        start_time.to_date.iso8601
+      end
+    end
+
+    def hours
+      if duration
+        duration
+      else
+        (end_time - start_time) / 3600
+      end
+    end
+
+    def == other
+      unless other.instance_of? Event
+        return false
+      end
+      task_id == other.task_id && spent_date == other.spent_date && title == other.title && hours == other.hours
     end
   end
 end
